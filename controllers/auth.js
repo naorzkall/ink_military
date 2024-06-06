c022d24d60a02fcf59e29c1de9220a0a422addbc
naorz kall <kallnaorz@gmail.com>
2024-06-06 20:57:40 +0300

    destroy session after logout,work myprofile frontend,remove some dummy codes

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,27 +29,6 @@ exports.getLogin = (req, res, next) => {
   });;
 };
 
-exports.getSignup = (req, res, next) => {
-  let message = req.flash('error');
-  if (message){
-    message = message[0];
-  }
-  else {
-    message = null;
-  }
-  res.render('auth/signup', {
-    path: '/signup',
-    pageTitle: 'Signup',
-    errorMessage: message,
-    oldInput: {
-      email: '',
-      password: '',
-      confirmPassword: ''
-    },
-    validationErrors: []
-  });
-};
-// new 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
@@ -117,51 +96,6 @@ exports.postLogin = (req, res, next) => {
       error.httpStatusCode = 500;
       return next(error);
     });
-};
-
-exports.postSignup = (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    console.log(errors.array());
-    return res.status(422).render('auth/signup', {
-      path: '/signup',
-      pageTitle: 'Signup',
-      errorMessage: errors.array()[0].msg,
-      oldInput: {
-        email: email,
-        password: password,
-        confirmPassword: req.body.confirmPassword
-      },
-      validationErrors: errors.array()
-    });
-  }
-  bcrypt
-  .hash(password, 12)
-  .then(hashedPassword => {
-    const user = new User({
-      email: email,
-      password: hashedPassword,
-      cart: { items: [] }
-    });
-    return user.save();
-  })
-  .then(result => {
-    res.redirect('/login');
-    return transporter.sendMail({
-      from: NODEJS_GMAIL_APP_USER, // sender address
-      to: email, // list of receivers
-      subject: 'Signup succeeded!', // Subject line
-      html: "<h1>you successfully signed up.</h1>"
-    });
-  })
-  .catch(err => {
-    const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
-  });          
 };
 
 exports.postLogout = (req, res, next) => {
--- routes/auth.js
+++ routes/auth.js
@@ -8,8 +8,6 @@ const router = express.Router();
 
 router.get('/login', authController.getLogin);
 
-router.get('/signup', authController.getSignup);
-
 router.post('/login',[
     check('email')
       .isEmail()
@@ -21,38 +19,6 @@ router.post('/login',[
       .trim()
   ],
  authController.postLogin);
-
-router.post('/signup',[
-    check('email')
-      .isEmail()
-      .withMessage('Please enter a valid email.')
-      .custom((value, { req }) => {
-        return User.findOne({ email: value }).then(userDoc => {
-          if (userDoc) {
-            return Promise.reject(
-              'E-Mail exists already, please pick a different one.'
-            );
-          }
-        });
-      })
-      .normalizeEmail(),
-    body(
-      'password',
-      'Please enter a password with only numbers and text and at least 5 characters.'
-    )
-      .isLength({ min: 5 })
-      .isAlphanumeric()
-      .trim(),
-    body('confirmPassword')
-      .trim()
-      .custom((value, { req }) => {
-        if (value !== req.body.password) {
-          throw new Error('Passwords have to match!');
-        }
-        return true;
-      })
-  ],
- authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
